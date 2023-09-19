f671e952634dcac66fe26d94bd13dbad73a7a2a9
brucelsprouts <134631582+brucelsprouts@users.noreply.github.com>
2023-09-19 09:19:56 -0400

    somebody fix this navigation..

--- main.js
+++ main.js
@@ -1,89 +1,58 @@
-// Store references to navigation items and content sections
-const navItems = document.querySelectorAll('.siteHeader_nav a');
-const contentSections = document.querySelectorAll('.page');
-const enterView = document.getElementById("EnterView");
-
-// Function to update the content based on the selected menu item
-function navigateToSection(targetURL) {
-    // Remove the 'active' class from all content sections
-    contentSections.forEach(section => section.classList.remove('active'));
-
-    // Show the content section corresponding to the target URL
-    const targetSection = document.querySelector(targetURL);
-    if (targetSection) {
-        targetSection.classList.add('active');
+document.addEventListener("DOMContentLoaded", function () {
+    // Get references to the navigation links
+    const homeLink = document.querySelector('a[href="index.html"]');
+    const codeProjectsLink = document.querySelector('a[href="codepj.html"]');
+    const afterEffectsProjectsLink = document.querySelector('a[href="aftereffectpj.html"]');
+    const photoshopProjectsLink = document.querySelector('a[href="photoshoppj.html"]');
+    const infoLink = document.querySelector('a[href="info.html"]');
+    const contactLink = document.querySelector('a[href="contact.html"]');
+  
+    // Get the main content container
+    const contentContainer = document.getElementById("Content");
+  
+    // Define a function to hide all sections
+    function hideAllSections() {
+      const sections = document.querySelectorAll(".page");
+      sections.forEach((section) => {
+        section.style.display = "none";
+      });
     }
-}
-
-// Function to handle navigation item clicks
-function handleNavigationClick(event) {
-    event.preventDefault();
-
-    // Remove the 'is-selected' class from all items
-    navItems.forEach(navItem => navItem.classList.remove('is-selected'));
-
-    // Add the 'is-selected' class to the clicked item
-    const item = event.target;
-    item.classList.add('is-selected');
-
-    // Get the target URL from the clicked item's href attribute
-    const targetURL = item.getAttribute('href');
-
-    // Update the content based on the target URL
-    navigateToSection(targetURL);
-}
-
-// Function to add smooth scrolling to anchor links
-function addSmoothScrolling(anchor) {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-
-        window.scrollTo({
-            top: targetElement.offsetTop,
-            behavior: 'smooth'
-        });
+  
+    // Define a function to show a specific section
+    function showSection(sectionId) {
+      hideAllSections();
+      const section = document.querySelector(`[data-page="${sectionId}"]`);
+      if (section) {
+        section.style.display = "block";
+      }
+    }
+  
+    // Add click event listeners to the navigation links
+    homeLink.addEventListener("click", () => {
+      showSection("home");
     });
-}
-
-// Function to change header background color on scroll
-function changeHeaderBackgroundColor() {
-    const header = document.querySelector('.siteHeader');
-    const headerHeight = header.clientHeight;
-
-    contentSections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        const sectionBottom = sectionTop + section.clientHeight;
-
-        if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
-            header.style.backgroundColor = section.getAttribute('data-header-bg-color');
-        }
+  
+    codeProjectsLink.addEventListener("click", () => {
+      showSection("codeprojects"); // Match the data-page attribute
     });
-}
-
-// Add click event listeners to navigation items
-navItems.forEach(item => {
-    item.addEventListener('click', handleNavigationClick);
-});
-
-// Add smooth scrolling to anchor links
-document.querySelectorAll('a[href^="#"]').forEach(addSmoothScrolling);
-
-// Function to remove the EnterView after fade-out animation
-function removeEnterView() {
-    enterView.style.display = "none";
-}
-
-// Add an event listener to remove EnterView after animation completes
-enterView.addEventListener("animationend", removeEnterView);
-
-// Add a scroll event listener for changing header background color
-window.addEventListener('scroll', changeHeaderBackgroundColor);
-
-// Initial page load: Show the Home page by default
-navigateToSection('#home');
-
-// Add the 'is-selected' class to the Home tab by default
-document.querySelector('[href="#home"]').classList.add('is-selected');
+  
+    afterEffectsProjectsLink.addEventListener("click", () => {
+      showSection("aftereffectprojects"); // Match the data-page attribute
+    });
+  
+    photoshopProjectsLink.addEventListener("click", () => {
+      showSection("photoshopprojects"); // Match the data-page attribute
+    });
+  
+    infoLink.addEventListener("click", () => {
+      showSection("info");
+    });
+  
+    contactLink.addEventListener("click", () => {
+      showSection("contact");
+    });
+  
+    // Initialize by showing the "home" section
+    showSection("home");
+  });
+  
