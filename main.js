2678a9332d11efeb4e60e039f8fc21d1fca999a3
brucelsprouts <134631582+brucelsprouts@users.noreply.github.com>
2023-09-18 22:18:14 -0400

    main updaye

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Store references to navigation items and content sections
 const navItems = document.querySelectorAll('.siteHeader_nav a');
 const contentSections = document.querySelectorAll('.page');
+const enterView = document.getElementById("EnterView");
 
 // Function to update the content based on the selected menu item
 function updateContent(targetURL) {
@@ -12,54 +13,58 @@ function updateContent(targetURL) {
     if (targetSection) targetSection.style.display = 'block';
 }
 
+// Function to handle navigation item clicks
+function handleNavigationClick(event) {
+    event.preventDefault();
+
+    // Remove the 'is-selected' class from all items
+    navItems.forEach(navItem => navItem.classList.remove('is-selected'));
+
+    // Add the 'is-selected' class to the clicked item
+    const item = event.target;
+    item.classList.add('is-selected');
+
+    // Get the target URL from the clicked item's href attribute
+    const targetURL = item.getAttribute('href');
+
+    // Update the content based on the target URL
+    updateContent(targetURL);
+
+    // If you want to close a mobile menu after clicking a link, you can add code here.
+}
+
+// Function to add smooth scrolling to anchor links
+function addSmoothScrolling(anchor) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+}
+
 // Add click event listeners to navigation items
 navItems.forEach(item => {
-    item.addEventListener('click', event => {
-        event.preventDefault();
-
-        // Remove the 'is-selected' class from all items
-        navItems.forEach(navItem => navItem.classList.remove('is-selected'));
-
-        // Add the 'is-selected' class to the clicked item
-        item.classList.add('is-selected');
-
-        // Get the target URL from the clicked item's href attribute
-        const targetURL = item.getAttribute('href');
-
-        // Update the content based on the target URL
-        updateContent(targetURL);
-
-        // If you want to close a mobile menu after clicking a link, you can add code here.
-    });
+    item.addEventListener('click', handleNavigationClick);
 });
 
-// Add smooth scrolling behavior for internal anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', e => {
-        e.preventDefault();
-        const targetId = anchor.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
-        if (targetSection) targetSection.scrollIntoView({ behavior: 'smooth' });
-    });
-});
+// Add smooth scrolling to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(addSmoothScrolling);
 
-// Initial page load: Show the Home page by default
-updateContent('#home');
+// Function to remove the EnterView after fade-out animation
+function removeEnterView() {
+    enterView.style.display = "none";
+}
 
-// Add the 'is-selected' class to the Home tab by default
-document.querySelector('[href="#home"]').classList.add('is-selected');
+// Add an event listener to remove EnterView after animation completes
+enterView.addEventListener("animationend", removeEnterView);
 
 // Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
-    // Get the EnterView element
-    const enterView = document.getElementById("EnterView");
-
-    // Function to remove the EnterView after fade-out animation
-    function removeEnterView() {
-        enterView.style.display = "none";
-    }
-
-    // Add an event listener to remove EnterView after animation completes
-    enterView.addEventListener("animationend", removeEnterView);
+    // Your additional initialization code can go here
 });
-
