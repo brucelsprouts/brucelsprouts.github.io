66cefb085f0ce151c221ff69ff929f371c743c4b
brucelsprouts <134631582+brucelsprouts@users.noreply.github.com>
2023-10-17 23:51:54 -0400

    dwads

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-        function openTab(tabName) {
-        var i;
-        var tabs = document.getElementsByClassName("tab");
-        for (i = 0; i < tabs.length; i++) {
-            tabs[i].style.display = "none";
-        }
-        document.getElementById(tabName).style.display = "block";
+function showPage(pageId) {
+    const pages = document.querySelectorAll('.content');
+    for (const page of pages) {
+        page.style.display = 'none';
     }
+    document.getElementById(pageId).style.display = 'block';
+}
 
-    function toggleMode() {
-        const body = document.body;
-        body.classList.toggle("dark-mode");
-        body.classList.toggle("light-mode");
-    }   
-});
+function lightDark() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    element.classList.toggle("light-mode");
+  }
