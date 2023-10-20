c7a049a019c77c3f1ed850c64e7aa65924b92494
brucelsprouts <134631582+brucelsprouts@users.noreply.github.com>
2023-10-20 12:50:12 -0400

    finished navigation + menu system

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ function lightDark() {
     element.classList.toggle("dark-mode");
     element.classList.toggle("light-mode");
 }
+
+function menuSwitch() {
+    var element = document.body;
+    element.classList.toggle("menu-show");
+    element.classList.toggle("menu-hide");
+}
+function menuHide() {
+    var element = document.body;
+    element.classList.remove("menu-show");
+    element.classList.add("menu-hide");
+}
