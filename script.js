7ca72f1289b3aa9ee6323282ce56c838132db32d
brucelsprouts <134631582+brucelsprouts@users.noreply.github.com>
2023-10-26 20:18:54 -0400

    dw

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function showPage(pageId) {
-    const pages = document.querySelectorAll('.content');
-    for (const page of pages) {
-        page.style.display = 'none';
-    }
-    document.getElementById(pageId).style.display = 'block';
-}
-
 function lightDark() {
     var element = document.body;
     element.classList.toggle("dark-mode");
