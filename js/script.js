2ca99941407b0e84d015cce42daf45b58c6655f4
brucelsprouts <134631582+brucelsprouts@users.noreply.github.com>
2024-10-19 22:55:30 -0400

    dasd

--- js/script.js
+++ js/script.js
@@ -47,8 +47,9 @@ function searchThing() {
 }
 
 function fadeInPage() {
-    if (!window.AnimationEvent) return;
-    document.getElementById('fader').classList.add('fade-out');
+    if (!window.AnimationEvent) { return; }
+    var fader = document.getElementById('fader');
+    fader.classList.add('fade-out');
 }
 
 function setupLinkFade() {
@@ -68,4 +69,30 @@ function setupLinkFade() {
 
 window.addEventListener('pageshow', event => {
     if (event.persisted) document.getElementById('fader').classList.remove('fade-in');
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    if (!window.AnimationEvent) { return; }
+    var anchors = document.getElementsByTagName('a');
+    
+    for (var idx = 0; idx < anchors.length; idx += 1) {
+        if (anchors[idx].hostname !== window.location.hostname ||
+            anchors[idx].pathname === window.location.pathname) {
+            continue;
+        }
+
+        anchors[idx].addEventListener('click', function(event) {
+            var fader = document.getElementById('fader'),
+                anchor = event.currentTarget;
+            
+            var listener = function() {
+                window.location = anchor.href;
+                fader.removeEventListener('animationend', listener);
+            };
+            fader.addEventListener('animationend', listener);
+            
+            event.preventDefault();
+            fader.classList.add('fade-in');
+        });
+    }
 });
